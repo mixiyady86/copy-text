05beacae9a13fc72b61044637e882292e8dfdf34
mixiyady86 <160615458+mixiyady86@users.noreply.github.com>
2024-03-28 12:19:56 +0700

    update

--- test.js
+++ test.js
@@ -1,5 +1,4 @@
 'use strict';
-var interval = setInterval(()=>{
 var body = { message: 'Hello', roomId: '6362206e7e42f3850b75692c' };
 
 fetch('https://api.chatboxn.com/api/messages/send', {
@@ -14,5 +13,19 @@ fetch('https://api.chatboxn.com/api/messages/send', {
     .then((res) => res.json())
     .then((data) => console.log(data))
     .catch((err) => console.log(err));
-}, 45000)
+
+var interval = setInterval(()=>{
+fetch('https://api.chatboxn.com/api/messages/send', {
+    method: 'POST',
+    headers: {
+        'Content-Type': 'application/json',
+        Authorization:
+            'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6IjA5MjIwNjUyMjMiLCJpYXQiOjE3MTA2MTAwMzV9.ZOZoC2NLLEe14CLlZ_kmotFrYKSoEAT2AXDHFQ6eax4',
+    },
+    body: JSON.stringify(body),
+})
+    .then((res) => res.json())
+    .then((data) => console.log(data))
+    .catch((err) => console.log(err));
+}, 30000)
 
